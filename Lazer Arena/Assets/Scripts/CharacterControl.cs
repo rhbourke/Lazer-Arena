6b41de82fa4024b903ea0cf456a6f05f6ceaedfc
AaronFrostDev <aaronfroststudent@gmail.com>
2018-03-26 05:34:59 -0400

    Successfully Impemented Character Controller
    Fixed every bug we had. Code could use some tidying up. The character movement is now not physics(RigidBody) based, and uses Unity's CharacterController component.

--- Lazer Arena/Assets/Scripts/CharacterControl.cs
+++ Lazer Arena/Assets/Scripts/CharacterControl.cs
@@ -6,7 +6,6 @@ using UnityEngine;
 public class CharacterControl : MonoBehaviour
 {
     Camera PlayerCam;
-    Rigidbody PlayerRB;
 
     [Header("FOV")]
 
@@ -19,7 +18,8 @@ public class CharacterControl : MonoBehaviour
 
     [Header("Movement")]
 
-    public static float personalSpeed = 10.0f; // The speed this character should travel at by default -- *This* variable should be adjusted based on class.
+    CharacterController charControl;
+    public static float personalSpeed = 1.0f; // The speed this character should travel at by default -- *This* variable should be adjusted based on class.
 
     [Range(1.2f, 3f)]
     public float sprintMultiplier = 1.5f;
@@ -28,29 +28,63 @@ public class CharacterControl : MonoBehaviour
     public float diagSpeed; // The divider by which the character should travel diagonally
 
     float speedMultiplier; 
-
     public bool canJump = true;
-    public static float jumpHeight = 2.0f;
-    public float jumpGravity = 10.0f;
+    public static float jumpHeight = 4.0f;
+    public float gravity;
+
+    float prevY;
 
     void Awake()
     {
         PlayerCam = gameObject.GetComponentInChildren<Camera>();
         PlayerCam.fieldOfView = startFOV;
-        PlayerRB = GetComponent<Rigidbody>();
-        PlayerRB.freezeRotation = true;
         speedMultiplier = personalSpeed;
     }
-
-    void FixedUpdate()
+    void Start()
     {
-        ///
-        /// First we will calculate the speedMultiplier 
-        ///
+        charControl = GetComponent<CharacterController>();
+    }
+    void Update()
+    {
+        
+        // Calculate multiplier based on current movement conditions(sprinting, diagonal movement... etc)
+        CalculateSpeedMultiplier();
 
+        // Calculate target movement 
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal") * speedMultiplier * 10, 0, Input.GetAxis("Vertical") * speedMultiplier * 10);
+        moveDirection.y = prevY;
+        if (charControl.isGrounded) {
+            prevY = 0.0f;
+            moveDirection = transform.TransformDirection(moveDirection);
+            if (canJump && Input.GetKeyDown(KeyCode.Space))
+            {
+                // Jump
+                Debug.Log("Jumped");
+                moveDirection.y = jumpHeight;
+            }
+        }
+        else
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal") * speedMultiplier * 10, moveDirection.y, Input.GetAxis("Vertical") * speedMultiplier * 10);
+            moveDirection = transform.TransformDirection(moveDirection);
+        }
+        
+        // Apply Gravity
+        moveDirection.y += gravity * Time.deltaTime;
 
+        // Store Y Vector
+        prevY = moveDirection.y;
+
+        // Apply target movement
+        charControl.Move(moveDirection * Time.deltaTime);
+        
+
+    }
+
+    void CalculateSpeedMultiplier()
+    {
         // If sprinting
-        if ((Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W)) && !(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))) 
+        if ((Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W)) && !(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)))
         {
             // Speed Up
             speedMultiplier = personalSpeed * sprintMultiplier;
@@ -65,12 +99,12 @@ public class CharacterControl : MonoBehaviour
             speedMultiplier = personalSpeed;
 
             // Shrink FOV
-            if(PlayerCam.fieldOfView > startFOV)
+            if (PlayerCam.fieldOfView > startFOV)
             {
                 PlayerCam.fieldOfView -= 1f * Time.fixedDeltaTime * transitDurToWalk * 100;
             }
 
-            if (((Input.GetKey(KeyCode.D) || (Input.GetKey(KeyCode.A))) && IsGrounded()))
+            if (((Input.GetKey(KeyCode.D) || (Input.GetKey(KeyCode.A))) && charControl.isGrounded))
             {
                 // Slow down while going diagonal
                 speedMultiplier = personalSpeed / diagSpeed;
@@ -78,44 +112,5 @@ public class CharacterControl : MonoBehaviour
             else
                 speedMultiplier = personalSpeed;
         }
-
-        ///
-        /// Now we use the speedMultiplier to move the character
-        ///
-
-        // Calculate how fast we should be moving on X and Y
-        Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal") * speedMultiplier, PlayerRB.velocity.y, (Input.GetAxis("Vertical")) * speedMultiplier);
-
-
-        // Apply the target velocity to the local space of the Rigidbody
-        PlayerRB.velocity = transform.TransformDirection(targetVelocity);
-
-
-        if (IsGrounded())
-        {
-            if (canJump && Input.GetButton("Jump"))
-            {
-                // Store the current velocity
-                Vector3 velocity = PlayerRB.velocity;
-                // Jump
-                PlayerRB.velocity = new Vector3(velocity.x, CalculateJumpVerticalSpeed(), velocity.y);
-            }
-        } 
-    }
-
-    bool IsGrounded()
-    {
-        // Checks for Raycast collision slightly under the player. ***Values may need future adjustment.***
-        if (Physics.Raycast(transform.position, new Vector3(0, -1.1f, 0), 1.1f)){
-            return true;
-        } else
-            return false;
-    }
-
-    float CalculateJumpVerticalSpeed()
-    {
-        // From the jump height and gravity we deduce the upwards speed 
-        // for the character to reach at the apex.
-        return Mathf.Sqrt(2 * jumpHeight * jumpGravity);
     }
 }
